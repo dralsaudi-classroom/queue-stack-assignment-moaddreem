6c0ecf5de050ade3f6ea5645b54dfc5a8515cd03
moaddreem <moaddreem0505@gmail.com>
2024-11-17 20:38:12 +0300

    Add files via upload
    this is just a test ( not my work <for now> )

--- src/main/java/com/example/project/QueueStackTester.java
+++ src/main/java/com/example/project/QueueStackTester.java
@@ -2,30 +2,70 @@ package com.example.project;
 
 public class QueueStackTester {
     public static <T> void split(Queue<T> q, Queue<T> oq, Queue<T> eq)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method split that splits a queue of n elements into two
-        // queues. The elements with odd orders (i.e. 1st, 3rd, 5th ...) should be put in the
-        // first queue and elements with even orders (i.e. 2nd, 4th, 6th ...) should be put in
-        // the second queue. The original queue should remain unchanged at the end of the
-        // method.
-        // Example. Given the queue (A, B, C, D, E), split results in oq (A, C, E), and eq(B, D).
+    {	
+    	if(q.length() < 1)
+    		return;
+    	if(oq.length() <= eq.length())
+    		oq.enqueue(q.serve());
+    	else {
+    		eq.enqueue(q.serve());
+    	}
+    		
+    	split(q , oq ,eq);
     }
     public static <T> void remove(LinkedPQ<T> pq, int p)
-    {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write a static method remove that removes every element in the priority queue
-        // having priority less than p.
-        // Example. Given pq: [A, 10], [D, 8], [B, 5], [E, 3], [C, 2] remove(pq, 5) results in
-        // pq: [A, 10], [D, 8], [B, 5].
+    {	
+    	
+    	LinkedPQ<T> tempQueue = new LinkedPQ<T>();
+    	PQElement<T> tempData;
+    	
+    	while(pq.length() != 0) {
+    		if((tempData = pq.serve()).p >= p )
+    			tempQueue.enqueue(tempData.data, tempData.p);
+    	}
+    	
+    	
+    	
+    	while(tempQueue.length() != 0) {
+    		tempData = tempQueue.serve();
+    		pq.enqueue(tempData.data, tempData.p);
+    	}
+    	
     }
     public static <T> boolean search(Stack<T> st, T e)
     {
-        throw new UnsupportedOperationException("Not supported yet.");
-        // Write the recursive static method search that searches for an element e in a stack st
-        // and returns true if it’s found or false otherwise. st should not change at the end of
-        // the method. You are not allowed to use any auxiliary data structures.
-        // Example. Given the stack st (top-to-bottom): 5, 7, 5, 3, 2. search(st, 3) returns
-        // true while search(st, 1) returns false.
+        if(st.empty())
+        	return false;
+        
+        T temp = st.pop();
+        if(e.equals(temp)) {
+        	st.push(temp);
+        	return true;
+        }
+        	
+        
+        boolean isFound = search(st,e);
+        
+        st.push(temp);
+        return isFound;
+ 
+    }
+    public static <T> void moveToEnd(List<T> l , int i) {
+    	l.findFirst();
+    	
+    	for(int j = 0 ; j < i ; j++) {
+    		if(l.last())
+    			return;
+    		l.findNext();
+    	}
+    	T tmp = l.retrieve();
+    	l.remove();
+    	
+    	while(!l.last()) {
+    		l.findNext();
+    	}
+    	
+    	l.insert(tmp);
+    	
     }
 }
